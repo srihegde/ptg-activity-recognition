3182c81f17441bd7ab563d0f7bf439845a0bc883
Sri Hegde <sri.hegde@krs-3021.khq.kitware.com>
2022-06-30 12:56:43 -0400

    Adding LSTM to temporal model

--- activity_hydra/src/models/components/smoke/engine/__init__.py
+++
@@ -1,2 +0,0 @@
-from .defaults import *
-from .launch import *
--- activity_hydra/src/models/components/smoke/modeling/detector/detectors.py
+++
@@ -1,5 +0,0 @@
-from .keypoint_detector import KeypointDetector
-
-
-def build_detection_model(cfg):
-    return KeypointDetector(cfg)
